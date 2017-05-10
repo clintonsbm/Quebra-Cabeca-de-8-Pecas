71f1e0de6c395fa4fedf97edaee5a56394dfaabf
Clinton de Sá Barreto Maciel <clintonsbm@gmail.com>
2017-05-10 14:45:12 -0300

    Initial Commit

---
+++ Quebra Cabeca de 8 Pecas/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Quebra Cabeca de 8 Pecas
+//
+//  Created by Clinton de Sá Barreto Maciel on 10/05/17.
+//  Copyright © 2017 Clinton Barreto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
